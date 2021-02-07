07e024c1ec207b92e05a973b79f6129a474812c6
PrithyRajNag <prithyrajnag.prn@gmail.com>
2021-02-07 20:02:53 +0600

    First Commit

---
+++ app/Http/Controllers/Namecontroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Namecontroller extends Controller
+{
+    public function name()
+    {
+        return "My name is Prithy Raj Nag";
+    }
+}
---
+++ app/Http/Controllers/VersityController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VersityController extends Controller
+{
+    public function versity()
+    {
+        return "I have completed my B.Sc in CSE from University of Science & Technology Chittagong(USTC)";
+    }
+}
